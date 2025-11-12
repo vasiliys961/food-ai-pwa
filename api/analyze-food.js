54cda047ddc6c9117953ed3e941ffe38aa80c870
vasiliys961 <vasily61@gmail.com>
2025-11-12 14:36:16 +0300

    Update analyze-food.js

--- api/analyze-food.js
+++ api/analyze-food.js
@@ -61,7 +61,7 @@ export default async function handler(req, res) {
         'content-type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'claude-3-haiku-20240307',
+        model: 'claude-3-5-sonnet-2024102',
         max_tokens: 1200,
         messages: [
           {
